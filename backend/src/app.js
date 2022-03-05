8b9deb8ec2f1cff63a3f08633e23e7e2111d732c
Christian Ghione <cghione@gmail.com>
2022-03-05 13:07:48 -0300

    ordeno index y app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,20 @@
+require('dotenv').config();// llama a las variables de entorno
+
 const express = require('express');
+const cors = require("cors");
 const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000)
+
+// middlewares
+app.use(express.urlencoded({extended: false})); //???
+app.use(cors());
+app.use(express.json()); //indico q use el format JSON de express
+
+//routes
+app.get("/", (req,res) => {
+    res.send("WELCOME TO MY API");
+  });
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,11 @@
-require('dotenv').config();// llama a las variables de entorno
 
 const app = require('./app');
 require ('./database');
 
 async function main() {
     
-    const port = process.env.PORT || 3000;//si quiero cambiar el puerto lo defino en el .env
-    await app.listen(port)
-    console.log('server en port: ', port);
+    await app.listen(app.get('port'));
+    console.log('server en port: ', app.get('port'));
 
 }
 
